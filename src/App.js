f8f45d51087fb1ed83e565fe1f58a7e039c4e081
Grigorii Sharapov <socaseinpoint@gmail.com>
2023-06-05 23:29:13 +0300

    feat: add web3-onboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react'
+import { init, useConnectWallet } from '@web3-onboard/react'
+import injectedModule from '@web3-onboard/injected-wallets'
+import { ethers } from 'ethers'
 import './App.css';
 
+// Sign up to get your free API key at https://explorer.blocknative.com/?signup=true
+// Required for Transaction Notifications and Transaction Preview
+const apiKey = '1730eff0-9d50-4382-a3fe-89f0d34a2070'
+
+const injected = injectedModule()
+
+const infuraKey = '10966ab1e04f407daad5237987bb9878'
+const rpcUrl = `https://mainnet.infura.io/v3/${infuraKey}`
+
+// initialize Onboard
+init({
+  apiKey,
+  wallets: [injected],
+  chains: [
+    {
+      id: '0x1',
+      token: 'ETH',
+      label: 'Ethereum Mainnet',
+      rpcUrl
+    }
+  ]
+})
+
 function App() {
+  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
+
+  // create an ethers provider
+  let ethersProvider
+
+  if (wallet) {
+    // if using ethers v6 this is:
+    // ethersProvider = new ethers.BrowserProvider(wallet.provider, 'any')
+    ethersProvider = new ethers.providers.Web3Provider(wallet.provider, 'any')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button disabled={connecting} onClick={() => (wallet ? disconnect(wallet) : connect())}>
+        {connecting ? 'connecting' : wallet ? 'disconnect' : 'connect'}
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
